82b1da6acc28ed658f64572019b7d216bb0ffbd7
Zunaid Ahmed <zunaidahmmed@gmail.com>
2024-01-15 23:33:58 +0600

    exam_app final orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 void main() {
   runApp(MyApp());
 }
@@ -9,23 +8,54 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('News Feed'),
+          backgroundColor: Colors.blue,
+        ),
+        body: OrientationBuilder(
+          builder: (context, orientation) {
+            return NewsFeed(orientation: orientation);
+          },
+        ),
+      ),
     );
   }
 }
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class NewsFeed extends StatelessWidget {
+  final Orientation orientation;
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
+  NewsFeed({required this.orientation});
 
-class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return ListView.builder(
+      itemCount: 10, // Number of news articles
+      itemBuilder: (context, index) {
+        return Card(
+          margin: EdgeInsets.all(8),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: ListTile(
+            leading: Container(
+              width: 150,
+              height: 150,
+              child: Center(
+                child: Image.network(
+                  'https://via.placeholder.com/150', // Replace with your actual image URL
+                  width: 100, // Set your preferred width
+                  height: 100, // Set your preferred height
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            title: Text('News Title $index'),
+            subtitle: Text('News Description $index'),
+          ),
+        );
+      },
+    );
   }
 }
-
